f096329f29e148e3bc86f3eab7893775e438400c
Samuel M.H <samuel.mh@gmail.com>
2013-02-12 17:39:38 +0100

    Added slug in admin for gallery identification

--- smh_gallery/admin.py
+++ smh_gallery/admin.py
@@ -2,13 +2,15 @@ from smh_gallery.models import Gallery, Image
 from django.contrib import admin
 
 class GalleryAdmin(admin.ModelAdmin):
-    fields = ['name','description']
-    
+    fields = ('slug','name','description')
+    list_display = ('name', 'slug')
+
 admin.site.register(Gallery, GalleryAdmin)
 
 
 class ImageAdmin(admin.ModelAdmin):
-    fields = ['name','image','description','date','gallery']
-    list_filter = ['gallery']
+    fields = ('name','image','description','date','gallery')
+    list_display = ('name',)
+    list_filter = ('gallery',)
 
 admin.site.register(Image, ImageAdmin)
