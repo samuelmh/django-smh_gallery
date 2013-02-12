67c969a86663aaf7c368f47aeba77bc24c1cc89e
Samuel M.H <samuel.mh@gmail.com>
2013-02-12 07:18:22 +0100

    Added 404 500 errorpages and gallery does not blink on load

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,14 @@
 
 #Django libs
 from django.shortcuts import render
-
+from django.template import TemplateDoesNotExist
+from django.http import Http404
 
 def static_section(request, section="home"):
     section = section
-    return (render(request, '{section}.html'.format(section=section), locals()))
+    try:
+        return (render(request, '{section}.html'.format(section=section), locals()))
+    except TemplateDoesNotExist:
+        raise Http404
+
 
