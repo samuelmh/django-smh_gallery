d21acffdcae4bd6a08621beedd93b5261cef3d89
Samuel M.H <samuel.mh@gmail.com>
2013-02-12 17:13:58 +0100

    fixed navbar and remove space

--- myapp/settings.py
+++ myapp/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/samuelmh/projects/django-smh_gallery/'
+STATIC_ROOT = BASE_PATH+'/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
